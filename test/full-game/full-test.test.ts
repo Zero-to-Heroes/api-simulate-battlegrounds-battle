23aa958e1a0452da32d7337f3ae8ae82e52e565c
sebastientromp <sebastien.tromp@gmail.com>
2020-11-27 09:24:14 +0100

    reduce memory and deactivate logs

--- test/full-game/full-test.test.ts
+++ test/full-game/full-test.test.ts
@@ -35,14 +35,14 @@ describe.skip('Full tests for performance and accuracy', () => {
 		const input: BgsBattleInfo = {
 			...jsonEvent3,
 			options: {
-				numberOfSimulations: 1,
+				numberOfSimulations: 2500,
 			},
 		};
 		SharedState.debugEnabled = false;
 		const result = await runSimulation({ 'body': JSON.stringify(input) });
 		const simulationResult = JSON.parse(result.body);
 
-		const sample = simulationResult.outcomeSamples.won[0];
+		const sample = simulationResult.outcomeSamples.tied[0];
 		const base64 = encode(JSON.stringify(sample));
 		console.log('encoded', base64);
 	});
