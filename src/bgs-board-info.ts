c081399d0f00d679701e38f8291c8a4a13b2c74b
sebastientromp <sebastien.tromp@gmail.com>
2021-05-10 15:39:05 +0200

    add board secrets to input
    Not used yet

--- src/bgs-board-info.ts
+++ src/bgs-board-info.ts
@@ -1,7 +1,9 @@
 import { BgsPlayerEntity } from './bgs-player-entity';
 import { BoardEntity } from './board-entity';
+import { BoardSecret } from './board-secret';
 
 export interface BgsBoardInfo {
 	readonly player: BgsPlayerEntity;
 	readonly board: BoardEntity[];
+	readonly secrets: BoardSecret[];
 }
---
+++ src/board-secret.ts
@@ -0,0 +1,4 @@
+export interface BoardSecret {
+	entityId: number;
+	cardId: string;
+}
