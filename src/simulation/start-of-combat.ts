9f7b125bb23a906e20da0e05709935d4afe919bd
sebastientromp <sebastien.tromp@gmail.com>
2022-02-03 20:43:29 +0100

    tentative fix for minion sim ordering

--- src/simulation/start-of-combat.ts
+++ src/simulation/start-of-combat.ts
@@ -14,10 +14,320 @@ import {
 	applyFireInvocationEnchantment,
 	applyLightningInvocationEnchantment,
 	applyWaterInvocationEnchantment,
-	dealDamageToAllMinions,
 } from './deathrattle-effects';
 import { SharedState } from './shared-state';
 import { Spectator } from './spectator/spectator';
+
+export const handleStartOfCombat = (
+	playerEntity: BgsPlayerEntity,
+	playerBoard: BoardEntity[],
+	opponentEntity: BgsPlayerEntity,
+	opponentBoard: BoardEntity[],
+	currentAttacker: number,
+	allCards: AllCardsService,
+	spawns: CardsData,
+	sharedState: SharedState,
+	gameState: BgsGameState,
+	spectator: Spectator,
+): number => {
+	// https://twitter.com/DCalkosz/status/1488361384320528388?s=20&t=1ECxRZFdjqwEa2fRsXk32Q
+	// There’s a certain order for Start of Combat hero powers, rather than “coin flips” where
+	// an unlucky trigger order could mess up some positioning you had planned for your own hero
+	// power. “Precombat” (Al’Akir, Y’Shaarj), then Illidan, then others.
+	currentAttacker = handlePreCombatHeroPowers(
+		playerEntity,
+		playerBoard,
+		opponentEntity,
+		opponentBoard,
+		currentAttacker,
+		allCards,
+		spawns,
+		sharedState,
+		gameState,
+		spectator,
+	);
+	currentAttacker = handleIllidanHeroPowers(
+		playerEntity,
+		playerBoard,
+		opponentEntity,
+		opponentBoard,
+		currentAttacker,
+		allCards,
+		spawns,
+		sharedState,
+		gameState,
+		spectator,
+	);
+	currentAttacker = handleStartOfCombatHeroPowers(
+		playerEntity,
+		playerBoard,
+		opponentEntity,
+		opponentBoard,
+		currentAttacker,
+		allCards,
+		spawns,
+		sharedState,
+		gameState,
+		spectator,
+	);
+	currentAttacker = handleStartOfCombatMinions(
+		playerEntity,
+		playerBoard,
+		opponentEntity,
+		opponentBoard,
+		currentAttacker,
+		allCards,
+		spawns,
+		sharedState,
+		gameState,
+		spectator,
+	);
+	return currentAttacker;
+};
+
+const handlePreCombatHeroPowers = (
+	playerEntity: BgsPlayerEntity,
+	playerBoard: BoardEntity[],
+	opponentEntity: BgsPlayerEntity,
+	opponentBoard: BoardEntity[],
+	currentAttacker: number,
+	allCards: AllCardsService,
+	spawns: CardsData,
+	sharedState: SharedState,
+	gameState: BgsGameState,
+	spectator: Spectator,
+): number => {
+	// Some are part of the incoming board: Y'Shaarj, Lich King
+	// Since the order is not important here, we just always do the player first
+	const playerHeroPowerId = playerEntity.heroPowerId || getHeroPowerForHero(playerEntity.cardId);
+	if (playerHeroPowerId === CardIds.SwattingInsectsBattlegrounds && playerBoard.length > 0) {
+		// Should be sent by the app, but it is an idempotent operation, so we can just reapply it here
+		handleAlakirForPlayer(playerBoard, playerEntity, opponentBoard, opponentEntity, allCards, spawns, sharedState, spectator);
+	}
+
+	const opponentHeroPowerId = opponentEntity.heroPowerId || getHeroPowerForHero(opponentEntity.cardId);
+	if (opponentHeroPowerId === CardIds.SwattingInsectsBattlegrounds && opponentBoard.length > 0) {
+		handleAlakirForPlayer(opponentBoard, opponentEntity, playerBoard, playerEntity, allCards, spawns, sharedState, spectator);
+	}
+
+	return currentAttacker;
+};
+
+export const handleIllidanHeroPowers = (
+	playerEntity: BgsPlayerEntity,
+	playerBoard: BoardEntity[],
+	opponentEntity: BgsPlayerEntity,
+	opponentBoard: BoardEntity[],
+	currentAttacker: number,
+	allCards: AllCardsService,
+	spawns: CardsData,
+	sharedState: SharedState,
+	gameState: BgsGameState,
+	spectator: Spectator,
+): number => {
+	// Apparently it's a toin coss about whether to handle Illidan first or Al'Akir first
+	// Auras are only relevant for Illidan, and already applied there
+	if (Math.random() < 0.5) {
+		currentAttacker = handlePlayerIllidanHeroPowers(
+			playerEntity,
+			playerBoard,
+			opponentEntity,
+			opponentBoard,
+			currentAttacker,
+			true,
+			allCards,
+			spawns,
+			sharedState,
+			spectator,
+		);
+		currentAttacker = handlePlayerIllidanHeroPowers(
+			opponentEntity,
+			opponentBoard,
+			playerEntity,
+			playerBoard,
+			currentAttacker,
+			false,
+			allCards,
+			spawns,
+			sharedState,
+			spectator,
+		);
+	} else {
+		currentAttacker = handlePlayerIllidanHeroPowers(
+			opponentEntity,
+			opponentBoard,
+			playerEntity,
+			playerBoard,
+			currentAttacker,
+			false,
+			allCards,
+			spawns,
+			sharedState,
+			spectator,
+		);
+		currentAttacker = handlePlayerIllidanHeroPowers(
+			playerEntity,
+			playerBoard,
+			opponentEntity,
+			opponentBoard,
+			currentAttacker,
+			true,
+			allCards,
+			spawns,
+			sharedState,
+			spectator,
+		);
+	}
+	return currentAttacker;
+};
+
+const handleStartOfCombatMinions = (
+	playerEntity: BgsPlayerEntity,
+	playerBoard: BoardEntity[],
+	opponentEntity: BgsPlayerEntity,
+	opponentBoard: BoardEntity[],
+	currentAttacker: number,
+	allCards: AllCardsService,
+	spawns: CardsData,
+	sharedState: SharedState,
+	gameState: BgsGameState,
+	spectator: Spectator,
+): number => {
+	let attackerForStart = Math.round(Math.random());
+	const playerAttackers = playerBoard.filter((entity) => spawns.startOfCombats.indexOf(entity.cardId) !== -1);
+	const opponentAttackers = opponentBoard.filter((entity) => spawns.startOfCombats.indexOf(entity.cardId) !== -1);
+	while (playerAttackers.length > 0 || opponentAttackers.length > 0) {
+		if (attackerForStart === 0 && playerAttackers.length > 0) {
+			const attacker = playerAttackers.splice(0, 1)[0];
+			performStartOfCombatMinionsForPlayer(
+				attacker,
+				playerBoard,
+				playerEntity,
+				opponentBoard,
+				opponentEntity,
+				allCards,
+				spawns,
+				sharedState,
+				gameState,
+				spectator,
+			);
+		} else if (attackerForStart === 1 && opponentAttackers.length > 0) {
+			const attacker = opponentAttackers.splice(0, 1)[0];
+			performStartOfCombatMinionsForPlayer(
+				attacker,
+				opponentBoard,
+				opponentEntity,
+				playerBoard,
+				playerEntity,
+				allCards,
+				spawns,
+				sharedState,
+				gameState,
+				spectator,
+			);
+		}
+		attackerForStart = (attackerForStart + 1) % 2;
+	}
+	return currentAttacker;
+};
+
+export const handleStartOfCombatHeroPowers = (
+	playerEntity: BgsPlayerEntity,
+	playerBoard: BoardEntity[],
+	opponentEntity: BgsPlayerEntity,
+	opponentBoard: BoardEntity[],
+	currentAttacker: number,
+	allCards: AllCardsService,
+	cardsData: CardsData,
+	sharedState: SharedState,
+	gameState: BgsGameState,
+	spectator: Spectator,
+): number => {
+	const attackingHeroPowerId = playerEntity.heroPowerId || getHeroPowerForHero(playerEntity.cardId);
+	const defendingHeroPowerId = opponentEntity.heroPowerId || getHeroPowerForHero(opponentEntity.cardId);
+	const numberOfDeathwingPresents =
+		(attackingHeroPowerId === CardIds.AllWillBurnBattlegrounds ? 1 : 0) +
+		(defendingHeroPowerId === CardIds.AllWillBurnBattlegrounds ? 1 : 0);
+	applyAuras(playerBoard, numberOfDeathwingPresents, cardsData, allCards);
+	applyAuras(opponentBoard, numberOfDeathwingPresents, cardsData, allCards);
+
+	// Apparently it's a toin coss about whether to handle Illidan first or Al'Akir first
+	// Auras are only relevant for Illidan, and already applied there
+	if (Math.random() < 0.5) {
+		currentAttacker = handlePlayerStartOfCombatHeroPowers(
+			playerEntity,
+			playerBoard,
+			opponentEntity,
+			opponentBoard,
+			currentAttacker,
+			true,
+			allCards,
+			cardsData,
+			sharedState,
+			spectator,
+		);
+		currentAttacker = handlePlayerStartOfCombatHeroPowers(
+			opponentEntity,
+			opponentBoard,
+			playerEntity,
+			playerBoard,
+			currentAttacker,
+			false,
+			allCards,
+			cardsData,
+			sharedState,
+			spectator,
+		);
+	} else {
+		currentAttacker = handlePlayerStartOfCombatHeroPowers(
+			opponentEntity,
+			opponentBoard,
+			playerEntity,
+			playerBoard,
+			currentAttacker,
+			false,
+			allCards,
+			cardsData,
+			sharedState,
+			spectator,
+		);
+		currentAttacker = handlePlayerStartOfCombatHeroPowers(
+			playerEntity,
+			playerBoard,
+			opponentEntity,
+			opponentBoard,
+			currentAttacker,
+			true,
+			allCards,
+			cardsData,
+			sharedState,
+			spectator,
+		);
+	}
+	removeAuras(playerBoard, cardsData);
+	removeAuras(opponentBoard, cardsData);
+	return currentAttacker;
+};
+
+const handlePlayerIllidanHeroPowers = (
+	playerEntity: BgsPlayerEntity,
+	playerBoard: BoardEntity[],
+	opponentEntity: BgsPlayerEntity,
+	opponentBoard: BoardEntity[],
+	currentAttacker: number,
+	friendly: boolean,
+	allCards: AllCardsService,
+	spawns: CardsData,
+	sharedState: SharedState,
+	spectator: Spectator,
+): number => {
+	const playerHeroPowerId = playerEntity.heroPowerId || getHeroPowerForHero(playerEntity.cardId);
+	if (playerHeroPowerId === CardIds.WingmenBattlegrounds && playerBoard.length > 0) {
+		handleIllidanForPlayer(playerBoard, playerEntity, opponentBoard, opponentEntity, allCards, spawns, sharedState, spectator);
+		currentAttacker = (currentAttacker + 1) % 2;
+	}
+	return currentAttacker;
+};
 
 const handleIllidanForPlayer = (
 	playerBoard: BoardEntity[],
@@ -107,59 +417,6 @@ const handleTamsinForPlayer = (
 	chosenEntity.definitelyDead = true;
 };
 
-// const handleYShaarj = (
-// 	playerBoard: BoardEntity[],
-// 	playerBoardHero: BgsPlayerEntity,
-// 	tavernTier: number,
-// 	friendly: boolean,
-// 	allCards: AllCardsService,
-// 	spawns: CardsData,
-// 	sharedState: SharedState,
-// 	spectator: Spectator,
-// ): void => {
-// 	const spawnedEntities = spawnEntities(
-// 		spawns.forTavernTier(tavernTier),
-// 		1,
-// 		playerBoard,
-// 		playerBoardHero,
-// 		allCards,
-// 		sharedState,
-// 		friendly,
-// 		false,
-// 	);
-// 	// Assume it goes last
-// 	playerBoard.push(...spawnedEntities);
-// };
-
-const handleNefarian = (
-	playerBoard: BoardEntity[],
-	playerBoardHero: BgsPlayerEntity,
-	opponentBoard: BoardEntity[],
-	opponentBoardHero: BgsPlayerEntity,
-	allCards: AllCardsService,
-	spawns: CardsData,
-	sharedState: SharedState,
-	spectator: Spectator,
-): void => {
-	// Theoretically we need to pass the player's board, in case this kills any enemy
-	// minion that can interact with the player board
-	// However, there are no such minions with 1 health
-	dealDamageToAllMinions(opponentBoard, opponentBoardHero, [], playerBoardHero, null, 1, allCards, spawns, sharedState, spectator);
-};
-
-// const handleLichKing = (playerBoard: BoardEntity[]): void => {
-// 	const nonRebornMinions = playerBoard.filter((minion) => !minion.reborn);
-// 	if (nonRebornMinions.length > 0) {
-// 		const targetReborn = nonRebornMinions[Math.floor(Math.random() * nonRebornMinions.length)];
-// 		targetReborn.reborn = true;
-// 	}
-// };
-
-// const handlePutricide = (playerBoard: BoardEntity[]): void => {
-// 	const target = playerBoard[Math.floor(Math.random() * playerBoard.length)];
-// 	target.attack = target.attack + 10;
-// };
-
 const handlePlayerStartOfCombatHeroPowers = (
 	playerEntity: BgsPlayerEntity,
 	playerBoard: BoardEntity[],
@@ -172,15 +429,8 @@ const handlePlayerStartOfCombatHeroPowers = (
 	sharedState: SharedState,
 	spectator: Spectator,
 ): number => {
-	// Lich King should be handled in the incoming board state
 	const playerHeroPowerId = playerEntity.heroPowerId || getHeroPowerForHero(playerEntity.cardId);
-	if (playerHeroPowerId === CardIds.SwattingInsectsBattlegrounds && playerBoard.length > 0) {
-		// Should be sent by the app, but it is an idempotent operation, so we can just reapply it here
-		handleAlakirForPlayer(playerBoard, playerEntity, opponentBoard, opponentEntity, allCards, spawns, sharedState, spectator);
-	} else if (playerHeroPowerId === CardIds.WingmenBattlegrounds && playerBoard.length > 0) {
-		handleIllidanForPlayer(playerBoard, playerEntity, opponentBoard, opponentEntity, allCards, spawns, sharedState, spectator);
-		currentAttacker = (currentAttacker + 1) % 2;
-	} else if (playerHeroPowerId === CardIds.TamsinRoame_FragrantPhylactery) {
+	if (playerHeroPowerId === CardIds.TamsinRoame_FragrantPhylactery) {
 		handleTamsinForPlayer(playerBoard, playerEntity, opponentBoard, opponentEntity, allCards, spawns, sharedState, spectator);
 	} else if (playerHeroPowerId === CardIds.AimLeftToken) {
 		const target = opponentBoard[0];
@@ -265,128 +515,6 @@ const handlePlayerStartOfCombatHeroPowers = (
 			spectator,
 		);
 	}
-	// else if (
-	// 	playerHeroPowerId === CardIds.SwattingInsectsBattlegrounds &&
-	// 	playerBoard.length > 0
-	// ) {
-	// }
-	// Will be sent by the client
-	// else if (
-	// 	playerEntity.heroPowerUsed &&
-	// 	playerHeroPowerId === CardIds.EmbraceYourRageBattlegrounds &&
-	// 	playerBoard.length < 7
-	// ) {
-	// 	handleYShaarj(playerBoard, playerEntity, playerEntity.tavernTier, friendly, allCards, spawns, sharedState, spectator);
-	// }
-	// else if (playerEntity.heroPowerUsed && playerHeroPowerId === CardIds.NefariousFireBattlegrounds && playerBoard.length > 0) {
-	// 	handleNefarian(playerBoard, playerEntity, opponentBoard, opponentEntity, allCards, spawns, sharedState, spectator);
-	// }
-	return currentAttacker;
-};
-
-export const handleStartOfCombat = (
-	playerEntity: BgsPlayerEntity,
-	playerBoard: BoardEntity[],
-	opponentEntity: BgsPlayerEntity,
-	opponentBoard: BoardEntity[],
-	currentAttacker: number,
-	allCards: AllCardsService,
-	spawns: CardsData,
-	sharedState: SharedState,
-	gameState: BgsGameState,
-	spectator: Spectator,
-): number => {
-	// Apparently it's a toin coss about whether to handle Illidan first or Al'Akir first
-	// Auras are only relevant for Illidan, and already applied there
-	if (Math.random() < 0.5) {
-		currentAttacker = handlePlayerStartOfCombatHeroPowers(
-			playerEntity,
-			playerBoard,
-			opponentEntity,
-			opponentBoard,
-			currentAttacker,
-			true,
-			allCards,
-			spawns,
-			sharedState,
-			spectator,
-		);
-		currentAttacker = handlePlayerStartOfCombatHeroPowers(
-			opponentEntity,
-			opponentBoard,
-			playerEntity,
-			playerBoard,
-			currentAttacker,
-			false,
-			allCards,
-			spawns,
-			sharedState,
-			spectator,
-		);
-	} else {
-		currentAttacker = handlePlayerStartOfCombatHeroPowers(
-			opponentEntity,
-			opponentBoard,
-			playerEntity,
-			playerBoard,
-			currentAttacker,
-			false,
-			allCards,
-			spawns,
-			sharedState,
-			spectator,
-		);
-		currentAttacker = handlePlayerStartOfCombatHeroPowers(
-			playerEntity,
-			playerBoard,
-			opponentEntity,
-			opponentBoard,
-			currentAttacker,
-			true,
-			allCards,
-			spawns,
-			sharedState,
-			spectator,
-		);
-	}
-
-	let attackerForStart = Math.round(Math.random());
-
-	const playerAttackers = playerBoard.filter((entity) => spawns.startOfCombats.indexOf(entity.cardId) !== -1);
-	const opponentAttackers = opponentBoard.filter((entity) => spawns.startOfCombats.indexOf(entity.cardId) !== -1);
-	while (playerAttackers.length > 0 || opponentAttackers.length > 0) {
-		if (attackerForStart === 0 && playerAttackers.length > 0) {
-			const attacker = playerAttackers.splice(0, 1)[0];
-			performStartOfCombat(
-				attacker,
-				playerBoard,
-				playerEntity,
-				opponentBoard,
-				opponentEntity,
-				allCards,
-				spawns,
-				sharedState,
-				gameState,
-				spectator,
-			);
-		} else if (attackerForStart === 1 && opponentAttackers.length > 0) {
-			const attacker = opponentAttackers.splice(0, 1)[0];
-			performStartOfCombat(
-				attacker,
-				opponentBoard,
-				opponentEntity,
-				playerBoard,
-				playerEntity,
-				allCards,
-				spawns,
-				sharedState,
-				gameState,
-				spectator,
-			);
-		}
-		attackerForStart = (attackerForStart + 1) % 2;
-	}
-	// TODO: update that in case of Illidan's HP
 	return currentAttacker;
 };
 
@@ -404,7 +532,7 @@ export const getHeroPowerForHero = (heroCardId: string): string => {
 	return null;
 };
 
-export const performStartOfCombat = (
+export const performStartOfCombatMinionsForPlayer = (
 	attacker: BoardEntity,
 	attackingBoard: BoardEntity[],
 	attackingBoardHero: BgsPlayerEntity,
