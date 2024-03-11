972a789bf3004b015a0f08f8c39383a5ae4e1403
sebastientromp <sebastien.tromp@gmail.com>
2024-03-11 09:06:18 +0100

    fix matador not properly removing new target's divine shield

--- src/simulation/attack.ts
+++ src/simulation/attack.ts
@@ -1040,9 +1040,29 @@ export const bumpEntities = (
 	) {
 		entity.abiityChargesLeft--;
 		const newTarget = pickRandom(otherBoard);
+		gameState.spectator.registerPowerTarget(entity, newTarget, otherBoard, entityBoardHero, otherHero);
 		// TODO: here the MadMatador is the source of the damage, not the initial attacker
 		// Not sure exactly what the impact would be, as there is no counter
-		return bumpEntities(newTarget, bumpInto, otherBoard, otherHero, entityBoard, entityBoardHero, gameState, false);
+		const newSource = {
+			...entity,
+			attack: bumpInto.attack,
+			attacking: true,
+		} as BoardEntity;
+		const defenderHadDivineShield = newTarget.divineShield;
+		const damageDone = bumpEntities(
+			newTarget,
+			newSource,
+			otherBoard,
+			otherHero,
+			entityBoard,
+			entityBoardHero,
+			gameState,
+			false,
+		);
+		if (newSource.attack > 0 && defenderHadDivineShield) {
+			updateDivineShield(newTarget, otherBoard, false, gameState.allCards);
+		}
+		return damageDone;
 	}
 
 	const damageDealt = (entity.damageMultiplier || 1) * bumpInto.attack;
