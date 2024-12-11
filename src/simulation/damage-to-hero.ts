1f3e94edeebd82c27afefd88ef9b3ff649ddf321
sebastientromp <sebastien.tromp@gmail.com>
2024-12-11 15:31:41 +0100

    revert hero damage if Soul Rewinder or Archimonde is on board

--- src/simulation/damage-to-hero.ts
+++ src/simulation/damage-to-hero.ts
@@ -1,6 +1,14 @@
+import { CardIds } from '@firestone-hs/reference-data';
 import { BgsPlayerEntity } from '../bgs-player-entity';
 import { BoardEntity } from '../board-entity';
 import { FullGameState } from './internal-game-state';
+
+const REWIND_HERO_DAMAGE_CARDS = [
+	CardIds.SoulRewinder_BG26_174,
+	CardIds.SoulRewinder_BG26_174_G,
+	CardIds.Archimonde_BG31_873,
+	CardIds.Archimonde_BG31_873_G,
+];
 
 export const dealDamageToHero = (
 	source: BoardEntity,
@@ -9,5 +17,8 @@ export const dealDamageToHero = (
 	damage: number,
 	gameState: FullGameState,
 ) => {
+	if (board.some((e) => REWIND_HERO_DAMAGE_CARDS.includes(e.cardId as CardIds))) {
+		return;
+	}
 	hero.hpLeft = hero.hpLeft - damage;
 };
