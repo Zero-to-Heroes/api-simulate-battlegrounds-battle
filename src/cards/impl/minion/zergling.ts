96280d3e4414e32530fd103fa88a1c793a2d4886
sebastientromp <sebastien.tromp@gmail.com>
2025-02-20 10:44:15 +0100

    fix golden zergling

--- src/cards/impl/minion/zergling.ts
+++ src/cards/impl/minion/zergling.ts
@@ -11,38 +11,41 @@ export const Zergling: StartOfCombatCard = {
 	cardIds: [CardIds.KerriganQueenOfBlades_ZerglingToken_BG31_HERO_811t2, CardIds.Zergling_BG31_HERO_811t2_G],
 	startOfCombat: (minion: BoardEntity, input: SoCInput) => {
 		let hasTriggered = false;
-		if (input.playerBoard.length < 7) {
-			const copy = copyEntity(minion);
-			removeAurasFromSelf(copy, input.playerBoard, input.playerEntity, input.gameState);
-			const newMinions = spawnEntities(
-				copy.cardId,
-				1,
-				input.playerBoard,
-				input.playerEntity,
-				input.opponentBoard,
-				input.opponentEntity,
-				input.gameState.allCards,
-				input.gameState.cardsData,
-				input.gameState.sharedState,
-				input.gameState.spectator,
-				input.playerEntity.friendly,
-				false,
-				false,
-				false,
-				copy,
-			);
-			const indexFromRight = input.playerBoard.length - input.playerBoard.indexOf(minion) - 1;
-			const spawns = performEntitySpawns(
-				newMinions,
-				input.playerBoard,
-				input.playerEntity,
-				input.playerEntity,
-				indexFromRight,
-				input.opponentBoard,
-				input.opponentEntity,
-				input.gameState,
-			);
-			hasTriggered = true;
+		const loops = minion.cardId === CardIds.Zergling_BG31_HERO_811t2_G ? 2 : 1;
+		for (let i = 0; i < loops; i++) {
+			if (input.playerBoard.length < 7) {
+				const copy = copyEntity(minion);
+				removeAurasFromSelf(copy, input.playerBoard, input.playerEntity, input.gameState);
+				const newMinions = spawnEntities(
+					copy.cardId,
+					1,
+					input.playerBoard,
+					input.playerEntity,
+					input.opponentBoard,
+					input.opponentEntity,
+					input.gameState.allCards,
+					input.gameState.cardsData,
+					input.gameState.sharedState,
+					input.gameState.spectator,
+					input.playerEntity.friendly,
+					false,
+					false,
+					false,
+					copy,
+				);
+				const indexFromRight = input.playerBoard.length - input.playerBoard.indexOf(minion) - 1;
+				const spawns = performEntitySpawns(
+					newMinions,
+					input.playerBoard,
+					input.playerEntity,
+					input.playerEntity,
+					indexFromRight,
+					input.opponentBoard,
+					input.opponentEntity,
+					input.gameState,
+				);
+				hasTriggered = true;
+			}
 		}
 		return { hasTriggered: hasTriggered, shouldRecomputeCurrentAttacker: hasTriggered };
 	},
