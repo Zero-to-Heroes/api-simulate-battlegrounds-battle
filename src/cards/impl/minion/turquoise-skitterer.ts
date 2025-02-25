9e3645e29dafa8618f9019c4d4b9bb7f33e5dd24
sebastientromp <sebastien.tromp@gmail.com>
2025-02-25 10:46:32 +0100

    fix turquoise skittere buff values

--- src/cards/impl/minion/turquoise-skitterer.ts
+++ src/cards/impl/minion/turquoise-skitterer.ts
@@ -5,6 +5,9 @@ import { simplifiedSpawnEntities } from '../../../simulation/deathrattle-spawns'
 import { modifyStats } from '../../../simulation/stats';
 import { DeathrattleEffectCard, DeathrattleSpawnCard } from '../../card.interface';
 
+const atkBuff = 1;
+const hpBuff = 1;
+
 export const TurquoiseSkitterer: DeathrattleSpawnCard & DeathrattleEffectCard = {
 	cardIds: [CardIds.TurquoiseSkitterer_BG31_809, CardIds.TurquoiseSkitterer_BG31_809_G],
 	deathrattleSpawn: (deadEntity: BoardEntity, input: DeathrattleTriggeredInput): readonly BoardEntity[] => {
@@ -14,9 +17,9 @@ export const TurquoiseSkitterer: DeathrattleSpawnCard & DeathrattleEffectCard = 
 	deathrattleEffect: (minion: BoardEntity, input: DeathrattleTriggeredInput) => {
 		const mult = minion.cardId === CardIds.TurquoiseSkitterer_BG31_809_G ? 2 : 1;
 		input.boardWithDeadEntityHero.globalInfo.BeetleAttackBuff =
-			input.boardWithDeadEntityHero.globalInfo.BeetleAttackBuff + 1 * mult;
+			input.boardWithDeadEntityHero.globalInfo.BeetleAttackBuff + atkBuff * mult;
 		input.boardWithDeadEntityHero.globalInfo.BeetleHealthBuff =
-			input.boardWithDeadEntityHero.globalInfo.BeetleHealthBuff + 1 * mult;
+			input.boardWithDeadEntityHero.globalInfo.BeetleHealthBuff + hpBuff * mult;
 		input.boardWithDeadEntity
 			.filter((e) =>
 				[CardIds.BoonOfBeetles_BeetleToken_BG28_603t, CardIds.Beetle_BG28_603t_G].includes(e.cardId as CardIds),
@@ -24,8 +27,8 @@ export const TurquoiseSkitterer: DeathrattleSpawnCard & DeathrattleEffectCard = 
 			.forEach((e) => {
 				modifyStats(
 					e,
-					1 * mult,
-					2 * mult,
+					atkBuff * mult,
+					hpBuff * mult,
 					input.boardWithDeadEntity,
 					input.boardWithDeadEntityHero,
 					input.gameState,
