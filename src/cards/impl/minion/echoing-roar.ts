385775436dd3d3d91e4222b13550a6f2ec82518b
sebastientromp <sebastien.tromp@gmail.com>
2025-04-08 14:44:23 +0200

    add support for multiple echoing roars

--- src/cards/impl/minion/echoing-roar.ts
+++ src/cards/impl/minion/echoing-roar.ts
@@ -6,7 +6,11 @@ import { EndOfTurnCard, EndOfTurnInput } from '../../card.interface';
 export const EchoingRoar: EndOfTurnCard = {
 	cardIds: [CardIds.EchoingRoar_EchoingRoarEnchantment_BG28_814e],
 	endOfTurn: (minion: BoardEntity, input: EndOfTurnInput) => {
-		modifyStats(minion, 2, 2, input.board, input.hero, input.gameState);
+		const enchant = minion.enchantments?.find(
+			(e) => e.cardId === CardIds.EchoingRoar_EchoingRoarEnchantment_BG28_814e,
+		);
+		const stats = enchant?.tagScriptDataNum1 ?? 1;
+		modifyStats(minion, 2 * stats, 2 * stats, input.board, input.hero, input.gameState);
 		input.gameState.spectator.registerPowerTarget(minion, minion, input.board, input.hero, input.otherHero);
 	},
 };
