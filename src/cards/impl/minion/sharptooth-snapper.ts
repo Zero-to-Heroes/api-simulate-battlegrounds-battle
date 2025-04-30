f95f88fa3b8ec87a701be9873211f9327bdb6527
sebastientromp <sebastien.tromp@gmail.com>
2025-04-30 08:36:29 +0200

    tentative fix for Sharptooth Snapper timing

--- src/cards/impl/minion/sharptooth-snapper.ts
+++ src/cards/impl/minion/sharptooth-snapper.ts
@@ -1,7 +1,17 @@
 import { CardIds } from '@firestone-hs/reference-data';
-import { DefaultScriptDataNumCard } from '../../card.interface';
+import { BoardEntity } from '../../../board-entity';
+import { SoCInput } from '../../../simulation/start-of-combat/start-of-combat-input';
+import { StartOfCombatCard } from '../../card.interface';
 
-export const SharptoothSnapper: DefaultScriptDataNumCard = {
+// Use this instead of DefaultScriptDataNumCard so that it doesn't trigger before the Start of Combat minions phase
+export const SharptoothSnapper: StartOfCombatCard = {
 	cardIds: [CardIds.SharptoothSnapper_BG32_201, CardIds.SharptoothSnapper_BG32_201_G],
-	defaultScriptDataNum: (cardId: string) => (cardId === CardIds.SharptoothSnapper_BG32_201_G ? 2 : 1),
+	startOfCombatTiming: 'start-of-combat',
+	startOfCombat: (
+		minion: BoardEntity,
+		input: SoCInput,
+	): { hasTriggered: boolean; shouldRecomputeCurrentAttacker: boolean } => {
+		minion.scriptDataNum1 = minion.cardId === CardIds.SharptoothSnapper_BG32_201_G ? 2 : 1;
+		return { hasTriggered: false, shouldRecomputeCurrentAttacker: false };
+	},
 };
