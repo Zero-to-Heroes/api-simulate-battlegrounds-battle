750942b6eed089e57db7e3510179dd726d935c79
sebastientromp <sebastien.tromp@gmail.com>
2025-05-02 08:38:59 +0200

    fix fire-forged evoker

--- src/cards/impl/minion/fire-forged-evoker.ts
+++ src/cards/impl/minion/fire-forged-evoker.ts
@@ -7,7 +7,8 @@ import { StartOfCombatCard } from '../../card.interface';
 export const FireForgedEvoker: StartOfCombatCard = {
 	cardIds: [CardIds.FireForgedEvoker_BG32_822, CardIds.FireForgedEvoker_BG32_822_G],
 	startOfCombat: (minion: BoardEntity, input: SoCInput) => {
-		const buff = minion.scriptDataNum1;
+		const mult = minion.cardId === CardIds.FireForgedEvoker_BG32_822_G ? 2 : 1;
+		const buff = mult + minion.scriptDataNum2;
 		addStatsToBoard(minion, input.playerBoard, input.playerEntity, buff, buff, input.gameState, Race[Race.DRAGON]);
 		return { hasTriggered: true, shouldRecomputeCurrentAttacker: false };
 	},
