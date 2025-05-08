579e916722b9deb078ea4e7af44cb5dc7788c8dc
sebastientromp <sebastien.tromp@gmail.com>
2025-05-08 17:15:45 +0200

    fix corrupted bristler

--- src/cards/impl/minion/corrupted-bristler.ts
+++ src/cards/impl/minion/corrupted-bristler.ts
@@ -7,32 +7,38 @@ import { DeathrattleSpawnCard } from '../../card.interface';
 export const CorruptedBristler: DeathrattleSpawnCard = {
 	cardIds: [CardIds.CorruptedBristler_BG32_431, CardIds.CorruptedBristler_BG32_431_G],
 	deathrattleSpawn: (minion: BoardEntity, input: DeathrattleTriggeredInput): readonly BoardEntity[] => {
+		// Looks like we only take the last enchantment into account
+		// See corrupted-bristler.jsonc
+		let bloodGemAttack = 0;
+		let bloodGemHealth = 0;
 		let bloodGemEnchantments = minion.enchantments?.filter(
 			(e) => e.cardId === CardIds.BloodGem_BloodGemEnchantment,
 		);
-		if (bloodGemEnchantments?.length === 0) {
+		if (!!bloodGemEnchantments?.length) {
+			const lastEnchantment = bloodGemEnchantments[bloodGemEnchantments.length - 1];
+			bloodGemAttack = lastEnchantment.tagScriptDataNum1 ?? 0;
+			bloodGemHealth = lastEnchantment.tagScriptDataNum2 ?? 0;
+		} else {
 			bloodGemEnchantments = minion.enchantments?.filter(
 				(e) => e.cardId === CardIds.BloodGem_BloodGemEnchantment,
 			);
+			bloodGemAttack = bloodGemEnchantments.map((e) => e.tagScriptDataNum1 ?? 0).reduce((a, b) => a + b, 0);
+			bloodGemHealth = bloodGemEnchantments.map((e) => e.tagScriptDataNum2 ?? 0).reduce((a, b) => a + b, 0);
 		}
 		const allSpawns = [];
-		if (bloodGemEnchantments?.length > 0) {
-			const bloodGemAttack = bloodGemEnchantments.map((e) => e.tagScriptDataNum1 ?? 0).reduce((a, b) => a + b, 0);
-			const bloodGemHealth = bloodGemEnchantments.map((e) => e.tagScriptDataNum2 ?? 0).reduce((a, b) => a + b, 0);
-			if (bloodGemAttack > 0 || bloodGemHealth > 0) {
-				const mult = minion.cardId === CardIds.CorruptedBristler_BG32_431_G ? 2 : 1;
-				for (let i = 0; i < mult; i++) {
-					const spawns = simplifiedSpawnEntities(
-						CardIds.BloodGolemSticker_BloodGolemToken_BG30_MagicItem_442t,
-						1,
-						input,
-					);
-					spawns.forEach((b) => {
-						b.attack = bloodGemAttack;
-						b.health = bloodGemHealth;
-					});
-					allSpawns.push(...spawns);
-				}
+		if (bloodGemAttack > 0 || bloodGemHealth > 0) {
+			const mult = minion.cardId === CardIds.CorruptedBristler_BG32_431_G ? 2 : 1;
+			for (let i = 0; i < mult; i++) {
+				const spawns = simplifiedSpawnEntities(
+					CardIds.BloodGolemSticker_BloodGolemToken_BG30_MagicItem_442t,
+					1,
+					input,
+				);
+				spawns.forEach((b) => {
+					b.attack = bloodGemAttack;
+					b.health = bloodGemHealth;
+				});
+				allSpawns.push(...spawns);
 			}
 		}
 		return allSpawns;
