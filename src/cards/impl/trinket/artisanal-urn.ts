fd4d8104e8322f8f4bb7883d07eed755ea75711a
sebastientromp <sebastien.tromp@gmail.com>
2025-05-09 18:28:37 +0200

    fix artisanal urn (lesser)

--- src/cards/impl/trinket/artisanal-urn.ts
+++ src/cards/impl/trinket/artisanal-urn.ts
@@ -4,10 +4,11 @@ import { SoCInput } from '../../../simulation/start-of-combat/start-of-combat-in
 import { StartOfCombatCard } from '../../card.interface';
 
 export const ArtisanalUrn: StartOfCombatCard = {
-	cardIds: [CardIds.ArtisanalUrn_ArtisanalUrnToken_BG30_MagicItem_989t],
+	cardIds: [CardIds.ArtisanalUrn_BG30_MagicItem_989, CardIds.ArtisanalUrn_ArtisanalUrnToken_BG30_MagicItem_989t],
 	startOfCombat: (trinket: BoardTrinket, input: SoCInput) => {
 		// Not sure why this is not handled as part of the undead attack bonus
-		input.playerEntity.globalInfo.UndeadAttackBonus += 8;
+		const buff = trinket.cardId === CardIds.ArtisanalUrn_BG30_MagicItem_989 ? 4 : 8;
+		input.playerEntity.globalInfo.UndeadAttackBonus += buff;
 		return { hasTriggered: true, shouldRecomputeCurrentAttacker: false };
 	},
 };
