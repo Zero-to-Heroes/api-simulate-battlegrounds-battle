05ab99ecce39ae1c95095e72a0bf1a23329cb303
sebastientromp <sebastien.tromp@gmail.com>
2025-05-10 17:23:21 +0200

    fix silithid burrower

--- src/cards/impl/minion/silithid-burrower.ts
+++ src/cards/impl/minion/silithid-burrower.ts
@@ -9,20 +9,20 @@ export const SilithidBurrower: DeathrattleSpawnCard & AvengeCard = {
 	cardIds: [CardIds.SilithidBurrower_BG29_871, CardIds.SilithidBurrower_BG29_871_G],
 	baseAvengeValue: (cardId: string) => 3,
 	deathrattleSpawn: (minion: BoardEntity, input: DeathrattleTriggeredInput) => {
-		const mult = minion.cardId === CardIds.SilithidBurrower_BG29_871_G ? 2 : 1;
 		const baseBuff = minion.scriptDataNum1 || 1;
 		addStatsToBoard(
 			minion,
 			input.boardWithDeadEntity,
 			input.boardWithDeadEntityHero,
-			baseBuff * mult,
-			baseBuff * mult,
+			baseBuff,
+			baseBuff,
 			input.gameState,
 			Race[Race.BEAST],
 		);
 		return [];
 	},
 	avenge: (minion: BoardEntity, input: AvengeInput) => {
-		minion.scriptDataNum1 = (minion.scriptDataNum1 || 1) + 1;
+		const mult = minion.cardId === CardIds.SilithidBurrower_BG29_871_G ? 2 : 1;
+		minion.scriptDataNum1 = (minion.scriptDataNum1 || 1) + 1 * mult;
 	},
 };
