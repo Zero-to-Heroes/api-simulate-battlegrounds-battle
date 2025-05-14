2f7a8b348992caa3f20db04a5a4458abbd67f9ea
sebastientromp <sebastien.tromp@gmail.com>
2025-05-14 08:58:18 +0200

    fix yulon sticker

--- src/cards/impl/trinket/yulon-sticker.ts
+++ src/cards/impl/trinket/yulon-sticker.ts
@@ -1,20 +1,29 @@
-import { CardIds } from '@firestone-hs/reference-data';
+import { CardIds, Race } from '@firestone-hs/reference-data';
 import { BoardEntity } from '../../../board-entity';
 import { shuffleArray } from '../../../services/utils';
 import { SoCInput } from '../../../simulation/start-of-combat/start-of-combat-input';
 import { makeMinionGolden } from '../../../simulation/utils/golden';
-import { isGolden } from '../../../utils';
+import { hasCorrectTribe, isGolden } from '../../../utils';
 import { StartOfCombatCard } from '../../card.interface';
 
 export const YulonSticker: StartOfCombatCard = {
 	cardIds: [CardIds.YulonSticker_BG32_MagicItem_419],
 	startOfCombat: (minion: BoardEntity, input: SoCInput) => {
-		const candidateBoard = input.playerBoard.filter((e) => !isGolden(e.cardId, input.gameState.allCards));
+		const candidateBoard = input.playerBoard.filter(
+			(e) =>
+				hasCorrectTribe(
+					e,
+					input.playerEntity,
+					Race.DRAGON,
+					input.gameState.anomalies,
+					input.gameState.allCards,
+				) && !isGolden(e.cardId, input.gameState.allCards),
+		);
 		// Because we pick one at random from all the ones that have the lowest tier
 		const randomBoard = shuffleArray(candidateBoard);
 		const candidates = randomBoard.sort(
 			(a, b) =>
-				input.gameState.cardsData.getTavernLevel(a.cardId) - input.gameState.cardsData.getTavernLevel(b.cardId),
+				input.gameState.cardsData.getTavernLevel(b.cardId) - input.gameState.cardsData.getTavernLevel(a.cardId),
 		);
 		const target = candidates[0];
 		if (!!target) {
