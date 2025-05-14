b3caf81f84ec9d8c794f44b2abe5dab229814e1e
sebastientromp <sebastien.tromp@gmail.com>
2025-05-14 09:25:11 +0200

    add comments for Silithid Burrower

--- src/cards/impl/minion/silithid-burrower.ts
+++ src/cards/impl/minion/silithid-burrower.ts
@@ -9,7 +9,13 @@ export const SilithidBurrower: DeathrattleSpawnCard & AvengeCard = {
 	cardIds: [CardIds.SilithidBurrower_BG29_871, CardIds.SilithidBurrower_BG29_871_G],
 	baseAvengeValue: (cardId: string) => 1,
 	deathrattleSpawn: (minion: BoardEntity, input: DeathrattleTriggeredInput) => {
-		const baseBuff = minion.scriptDataNum1 || 1;
+		// Not sure about which one to use
+		// https://replays.firestoneapp.com/?reviewId=f05e3b55-152b-4a6d-afb4-c3180d24cfdd&turn=23&action=6
+		// ^ in this, the Burrower has script1 = 29 and script2 = 1, and the minions get a +2 buff
+		// This seems to indicate that script2 is used? Or is it simply an issue where Silithid Burrower loses
+		// its permanent buffs, and script1 should be used?
+		const mult = minion.cardId === CardIds.SilithidBurrower_BG29_871_G ? 2 : 1;
+		const baseBuff = minion.scriptDataNum1 || mult;
 		addStatsToBoard(
 			minion,
 			input.boardWithDeadEntity,
