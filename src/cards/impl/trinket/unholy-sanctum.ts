169817d52b262951420dd99638f1d699d992d78f
sebastientromp <sebastien.tromp@gmail.com>
2025-05-14 08:44:45 +0200

    fix unholy sanctum

--- src/cards/impl/trinket/unholy-sanctum.ts
+++ src/cards/impl/trinket/unholy-sanctum.ts
@@ -7,14 +7,13 @@ import { DeathrattleTriggeredCard } from '../../card.interface';
 export const UnholySanctum: DeathrattleTriggeredCard = {
 	cardIds: [CardIds.UnholySanctum_BG32_MagicItem_862, CardIds.UnholySanctum_UnholySanctumToken_BG32_MagicItem_862t],
 	onDeathrattleTriggered: (trinket: BoardTrinket, input: DeathrattleTriggeredInput) => {
-		const mult = trinket.cardId === CardIds.UnholySanctum_BG32_MagicItem_862 ? 1 : 2;
 		const target = input.boardWithDeadEntity[input.boardWithDeadEntity.length - 1];
 		if (!!target) {
 			modifyStats(
 				target,
 				trinket,
-				3 * mult,
-				2 * mult,
+				trinket.cardId === CardIds.UnholySanctum_BG32_MagicItem_862 ? 3 : 6,
+				trinket.cardId === CardIds.UnholySanctum_BG32_MagicItem_862 ? 2 : 6,
 				input.boardWithDeadEntity,
 				input.boardWithDeadEntityHero,
 				input.gameState,
