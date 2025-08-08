72ae63d62190204a223fe5a5b2a4d141d501b853
sebastientromp <sebastien.tromp@gmail.com>
2025-08-08 18:52:07 +0200

    fix ornomonos

--- src/cards/impl/minion/oranomonos-the-wilted.ts
+++ src/cards/impl/minion/oranomonos-the-wilted.ts
@@ -1,12 +1,23 @@
-import { CardIds } from '@firestone-hs/reference-data';
+import { CardIds, Race } from '@firestone-hs/reference-data';
 import { BoardEntity } from '../../../board-entity';
 import { RebornEffectInput } from '../../../simulation/reborn';
 import { RebornEffectCard } from '../../card.interface';
+import { addStatsToBoard } from '../../../utils';
 
 export const OranomonosTheWilted: RebornEffectCard = {
 	cardIds: [CardIds.OranomonosTheWilted_BG33_116, CardIds.OranomonosTheWilted_BG33_116_G],
 	rebornEffect: (minion: BoardEntity, input: RebornEffectInput): void => {
 		const mult = minion.cardId === CardIds.OranomonosTheWilted_BG33_116_G ? 2 : 1;
-		input.opponentBoardHero.globalInfo.UndeadAttackBonus += 1 * mult;
+		input.boardWithKilledMinionHero.globalInfo.UndeadAttackBonus += 1 * mult;
+		addStatsToBoard(
+			minion,
+			input.boardWithKilledMinion,
+			input.boardWithKilledMinionHero,
+			1 * mult,
+			0,
+			input.gameState,
+			Race[Race.UNDEAD],
+			false,
+		);
 	},
 };
