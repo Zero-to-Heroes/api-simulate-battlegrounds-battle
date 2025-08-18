679d73ae663d3f80db159773a0aef57b24b41d49
sebastientromp <sebastien.tromp@gmail.com>
2025-08-18 15:30:58 +0200

    fix golden greenskeeper

--- src/cards/impl/minion/greenskeeper.ts
+++ src/cards/impl/minion/greenskeeper.ts
@@ -2,6 +2,7 @@ import { CardIds } from '@firestone-hs/reference-data';
 import { BoardEntity } from '../../../board-entity';
 import { hasMinionBattlecry, triggerBattlecry } from '../../../simulation/battlecries';
 import { OnAttackInput } from '../../../simulation/on-attack';
+import { isDead } from '../../../utils';
 import { OnAttackCard } from '../../card.interface';
 
 export const Greenskeeper: OnAttackCard = {
@@ -11,17 +12,22 @@ export const Greenskeeper: OnAttackCard = {
 			return { dmgDoneByAttacker: 0, dmgDoneByDefender: 0 };
 		}
 
-		const battlecries = input.attackingBoard.filter((e) => hasMinionBattlecry(e, input.gameState));
-		const candidate = battlecries[battlecries.length - 1];
-		if (!!candidate) {
-			triggerBattlecry(
-				input.attackingBoard,
-				input.attackingHero,
-				candidate,
-				input.defendingBoard,
-				input.defendingHero,
-				input.gameState,
+		const loops = minion.cardId === CardIds.Greenskeeper_BG30_008 ? 1 : 2;
+		for (let i = 0; i < loops; i++) {
+			const battlecries = input.attackingBoard.filter(
+				(e) => !isDead(e) && hasMinionBattlecry(e, input.gameState),
 			);
+			const candidate = battlecries[battlecries.length - 1];
+			if (!!candidate) {
+				triggerBattlecry(
+					input.attackingBoard,
+					input.attackingHero,
+					candidate,
+					input.defendingBoard,
+					input.defendingHero,
+					input.gameState,
+				);
+			}
 		}
 
 		return { dmgDoneByAttacker: 0, dmgDoneByDefender: 0 };
