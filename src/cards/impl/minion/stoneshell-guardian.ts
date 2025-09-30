a53e79477ee2cfe0847065b736bf59e885d4a153
sebastientromp <sebastien.tromp@gmail.com>
2025-09-30 18:08:33 +0200

    fix stoneshell guardian

--- src/cards/impl/minion/stoneshell-guardian.ts
+++ src/cards/impl/minion/stoneshell-guardian.ts
@@ -1,8 +1,6 @@
 import { GameTag } from '@firestone-hs/reference-data';
 import { BoardEntity } from '../../../board-entity';
 import { CardIds } from '../../../services/card-ids';
-import { spawnEntities } from '../../../simulation/deathrattle-spawns';
-import { performEntitySpawns } from '../../../simulation/spawns';
 import { SoCInput } from '../../../simulation/start-of-combat/start-of-combat-input';
 import { hasEntityMechanic } from '../../../utils';
 import { StartOfCombatCard } from '../../card.interface';
@@ -14,7 +12,7 @@ export const StoneshellGuardian: StartOfCombatCard = {
 		const numberOfCopies = minion.cardId === CardIds.StoneshellGuardian_BG33_HERO_000_Buddy_G ? 2 : 1;
 		for (let i = 0; i < numberOfCopies; i++) {
 			for (let j = 0; j < 2; j++) {
-				if (!!input.playerBoard.length && input.playerBoard.length < 7) {
+				if (!!input.playerBoard.length) {
 					const rallyMinions = input.playerBoard.filter(
 						(e) =>
 							!StoneshellGuardian.cardIds.includes(e.cardId) &&
@@ -24,39 +22,14 @@ export const StoneshellGuardian: StartOfCombatCard = {
 					if (!target) {
 						continue;
 					}
-					const copy: BoardEntity = {
-						...target,
-						lastAffectedByEntity: null,
-					};
-					const newMinions = spawnEntities(
-						copy.cardId,
-						1,
-						input.playerBoard,
-						input.playerEntity,
-						input.opponentBoard,
-						input.opponentEntity,
-						input.gameState,
-						target.friendly,
-						true,
-						false,
-						false,
-						copy,
-					);
-					const indexFromRight = input.playerBoard.length - (input.playerBoard.indexOf(target) + 1);
-					const actualSpawns = performEntitySpawns(
-						newMinions,
-						input.playerBoard,
-						input.playerEntity,
-						target,
-						indexFromRight,
-						input.opponentBoard,
-						input.opponentEntity,
-						input.gameState,
-					);
-					// totalSpawned += actualSpawns.length;
+					minion.enchantments.push({
+						cardId: target.cardId,
+						originEntityId: target.entityId,
+						timing: input.gameState.sharedState.currentEntityId++,
+					});
 					input.gameState.spectator.registerPowerTarget(
 						minion,
-						copy,
+						target,
 						input.playerBoard,
 						input.playerEntity,
 						input.opponentEntity,
--- test/full-game/apply-debug-state.ts
+++ test/full-game/apply-debug-state.ts
@@ -2,7 +2,7 @@ import { debugState } from '../../src/debug-state';
 
 export const applyDebugState = () => {
 	const state = debugState;
-	state.active = false;
-	state.forcedCurrentAttacker = 0;
+	state.active = true;
+	state.forcedCurrentAttacker = 1;
 	state.forcedFaceOffBase = [{ attacker: { entityId: 17960 }, defender: { entityId: 16215 } }];
 };
