5e958e3c3f4c176ed4a65053c7155525dff501d1
sebastientromp <sebastien.tromp@gmail.com>
2025-10-02 09:58:59 +0200

    fix kalecgos

--- src/cards/impl/minion/kalecgos-arcane-aspect.ts
+++ src/cards/impl/minion/kalecgos-arcane-aspect.ts
@@ -1,6 +1,6 @@
-import { CardIds } from '../../../services/card-ids';
 import { Race } from '@firestone-hs/reference-data';
 import { BoardEntity } from '../../../board-entity';
+import { CardIds } from '../../../services/card-ids';
 import { OnBattlecryTriggeredInput } from '../../../simulation/battlecries';
 import { addStatsToBoard } from '../../../utils';
 import { OnBattlecryTriggeredCard } from '../../card.interface';
@@ -9,6 +9,6 @@ export const KalecgosArcaneAspect: OnBattlecryTriggeredCard = {
 	cardIds: [CardIds.KalecgosArcaneAspect_BGS_041, CardIds.KalecgosArcaneAspect_TB_BaconUps_109],
 	onBattlecryTriggered: (minion: BoardEntity, input: OnBattlecryTriggeredInput) => {
 		const buff = minion.cardId === CardIds.KalecgosArcaneAspect_BGS_041 ? 1 : 2;
-		addStatsToBoard(minion, input.board, input.hero, buff, 2 * buff, input.gameState, Race[Race.DRAGON]);
+		addStatsToBoard(minion, input.board, input.hero, 1 * buff, 1 * buff, input.gameState, Race[Race.DRAGON]);
 	},
 };
--- test/full-game/apply-debug-state.ts
+++ test/full-game/apply-debug-state.ts
@@ -3,13 +3,9 @@ import { debugState } from '../../src/debug-state';
 export const applyDebugState = () => {
 	const state = debugState;
 	state.active = false;
-	state.forcedCurrentAttacker = 0;
+	state.forcedCurrentAttacker = 1;
 	state.forcedFaceOffBase = [
-		{ attacker: { entityId: 5023 }, defender: { entityId: 5887 } },
-		{ attacker: { entityId: 5878 }, defender: { entityId: 4271 } },
-		{ attacker: { entityId: 4259 }, defender: { entityId: 5890 } },
-		{ attacker: { entityId: 5880 }, defender: { entityId: 4271 } },
-		{ attacker: { entityId: 5880 }, defender: { entityId: 5308 } },
-		{ attacker: { entityId: 4265 }, defender: { entityId: 5880 } },
+		{ attacker: { entityId: 10095 }, defender: { entityId: 8733 } },
+		{ attacker: { entityId: 10095 }, defender: { entityId: 9021 } },
 	];
 };
