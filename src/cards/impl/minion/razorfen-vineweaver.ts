6ffd84ede11856951c7a1741e6e31c1c0242ee07
sebastientromp <sebastien.tromp@gmail.com>
2025-10-03 15:27:07 +0200

    fix razorfen vineweaver

--- src/cards/impl/minion/razorfen-vineweaver.ts
+++ src/cards/impl/minion/razorfen-vineweaver.ts
@@ -1,5 +1,5 @@
-import { CardIds } from '../../../services/card-ids';
 import { BoardEntity } from '../../../board-entity';
+import { CardIds } from '../../../services/card-ids';
 import { playBloodGemsOn } from '../../../simulation/blood-gems';
 import { OnAttackInput } from '../../../simulation/on-attack';
 import { RallyCard } from '../../card.interface';
@@ -8,7 +8,7 @@ export const RazorfenVineweaver: RallyCard = {
 	cardIds: [CardIds.RazorfenVineweaver_BG33_883, CardIds.RazorfenVineweaver_BG33_883_G],
 	rally: (minion: BoardEntity, input: OnAttackInput) => {
 		const mult = minion.cardId === CardIds.RazorfenVineweaver_BG33_883_G ? 2 : 1;
-		playBloodGemsOn(minion, minion, 2 * mult, input.attackingBoard, input.attackingHero, input.gameState);
+		playBloodGemsOn(minion, minion, 3 * mult, input.attackingBoard, input.attackingHero, input.gameState);
 		return { dmgDoneByAttacker: 0, dmgDoneByDefender: 0 };
 	},
 };
