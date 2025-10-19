d8f25ec88b0d985398d23d3cf8ab2f5a40eda0ef
sebastientromp <sebastien.tromp@gmail.com>
2025-10-19 23:08:43 +0200

    add onSpawned/Despawned to Malorne

--- src/cards/impl/minion/malorne.ts
+++ src/cards/impl/minion/malorne.ts
@@ -1,9 +1,10 @@
 import { BoardEntity } from '../../../board-entity';
 import { CardIds } from '../../../services/card-ids';
+import { OnDespawnInput, OnSpawnInput } from '../../../simulation/add-minion-to-board';
 import { RebornEffectInput } from '../../../simulation/reborn';
-import { RebornSelfEffectCard } from '../../card.interface';
+import { OnDespawnedCard, OnSpawnedCard, RebornSelfEffectCard } from '../../card.interface';
 
-export const Malorne: RebornSelfEffectCard = {
+export const Malorne: RebornSelfEffectCard & OnDespawnedCard & OnSpawnedCard = {
 	cardIds: [CardIds.Malorne_BG32_HERO_001_Buddy, CardIds.Malorne_BG32_HERO_001_Buddy_G],
 	rebornSelfEffect: (minion: BoardEntity, input: RebornEffectInput) => {
 		const mult = minion.cardId === CardIds.Malorne_BG32_HERO_001_Buddy_G ? 2 : 1;
@@ -11,5 +12,19 @@ export const Malorne: RebornSelfEffectCard = {
 		const baseBuff = Math.floor(totalGoldSpent / 3);
 		minion.attack += baseBuff * mult;
 		minion.health += baseBuff * mult;
+	},
+	onSpawned: (minion: BoardEntity, input: OnSpawnInput) => {
+		const mult = minion.cardId === CardIds.Malorne_BG32_HERO_001_Buddy_G ? 2 : 1;
+		const totalGoldSpent = input.hero.globalInfo.GoldSpentThisGame;
+		const baseBuff = Math.floor(totalGoldSpent / 3);
+		minion.attack += baseBuff * mult;
+		minion.health += baseBuff * mult;
+	},
+	onDespawned: (minion: BoardEntity, input: OnDespawnInput) => {
+		const mult = minion.cardId === CardIds.Malorne_BG32_HERO_001_Buddy_G ? 2 : 1;
+		const totalGoldSpent = input.hero.globalInfo.GoldSpentThisGame;
+		const baseBuff = Math.floor(totalGoldSpent / 3);
+		minion.attack = Math.max(0, minion.attack - baseBuff * mult);
+		minion.health = Math.max(1, minion.health - baseBuff * mult);
 	},
 };
