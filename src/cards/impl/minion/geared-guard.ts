aea2a4a2f5bfdb1c8af6f4a33257ba44b7e7eeea
sebastientromp <sebastien.tromp@gmail.com>
2025-10-25 08:10:51 +0200

    fix geared guard + stoneshell sentinel

--- src/cards/impl/minion/geared-guard.ts
+++ src/cards/impl/minion/geared-guard.ts
@@ -4,15 +4,17 @@ import { updateDivineShield } from '../../../keywords/divine-shield';
 import { pickRandom } from '../../../services/utils';
 import { OnAttackInput } from '../../../simulation/on-attack';
 import { RallyCard } from '../../card.interface';
+import { cardMappings } from '../_card-mappings';
 
 export const GearedGuard: RallyCard = {
 	cardIds: [CardIds.GearedGuard_BG33_325, CardIds.GearedGuard_BG33_325_G],
 	rally: (minion: BoardEntity, input: OnAttackInput) => {
 		const mult = minion.cardId === CardIds.GearedGuard_BG33_325_G ? 2 : 1;
+		// For when the Rally is triggered by another minion, eg Stoneshell Sentinel
+		// 33.6.2 https://replays.firestoneapp.com/?reviewId=68d6082e-f592-4028-b191-9268e3e6d08f&turn=31&action=2
+		const cardIds = cardMappings[input.attacker.cardId]?.cardIds ?? GearedGuard.cardIds;
 		for (let i = 0; i < mult; i++) {
-			const possibleTargets = input.attackingBoard.filter(
-				(e) => !GearedGuard.cardIds.includes(e.cardId) && !e.divineShield,
-			);
+			const possibleTargets = input.attackingBoard.filter((e) => !cardIds.includes(e.cardId) && !e.divineShield);
 			if (possibleTargets.length > 0) {
 				const target = pickRandom(possibleTargets);
 				updateDivineShield(
