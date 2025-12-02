b0d0a9a21af8afe40f007146a3d43e963ec8e3f7
sebastientromp <sebastien.tromp@gmail.com>
2025-12-02 21:59:15 +0100

    fix twilight hatchling

--- src/cards/impl/minion/twilight-hatchling.ts
+++ src/cards/impl/minion/twilight-hatchling.ts
@@ -5,13 +5,10 @@ import { simplifiedSpawnEntities } from '../../../simulation/deathrattle-spawns'
 import { DeathrattleSpawnCard } from '../../card.interface';
 
 export const TwilightHatchling: DeathrattleSpawnCard = {
-	cardIds: [
-		CardIds.TwilightHatchling_TwilightWhelpToken_BG34_630t,
-		CardIds.TwilightHatchling_TwilightWhelpToken_BG34_630_Gt,
-	],
+	cardIds: [CardIds.TwilightHatchling_BG34_630, CardIds.TwilightHatchling_BG34_630_G],
 	deathrattleSpawn: (minion: BoardEntity, input: DeathrattleTriggeredInput): readonly BoardEntity[] => {
-		const mult = minion.cardId === CardIds.TwilightHatchling_TwilightWhelpToken_BG34_630_Gt ? 2 : 1;
-		const spawns = simplifiedSpawnEntities(CardIds.TwilightWhelp, mult, input);
+		const mult = minion.cardId === CardIds.TwilightHatchling_BG34_630_G ? 2 : 1;
+		const spawns = simplifiedSpawnEntities(CardIds.TwilightHatchling_TwilightWhelpToken_BG34_630t, mult, input);
 		spawns.forEach((e) => {
 			e.attackImmediately = true;
 		});
