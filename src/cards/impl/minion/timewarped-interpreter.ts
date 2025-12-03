7ffa8e628233446581df8083ecf01b9a06c7fef0
sebastientromp <sebastien.tromp@gmail.com>
2025-12-03 08:59:35 +0100

    fix timewarped interpreter

--- src/cards/impl/minion/timewarped-interpreter.ts
+++ src/cards/impl/minion/timewarped-interpreter.ts
@@ -1,3 +1,4 @@
+import { Race } from '@firestone-hs/reference-data';
 import { BoardEntity } from '../../../board-entity';
 import { CardIds } from '../../../services/card-ids';
 import { OnBeforeMagnetizeInput } from '../../../simulation/magnetize';
@@ -8,6 +9,6 @@ export const TimewarpedInterpreter: OnBeforeMagnetizeCard = {
 	cardIds: [CardIds.TimewarpedInterpreter_BG34_Giant_120, CardIds.TimewarpedInterpreter_BG34_Giant_120_G],
 	onBeforeMagnetize: (entity: BoardEntity, input: OnBeforeMagnetizeInput) => {
 		const mult = entity.cardId === CardIds.TimewarpedInterpreter_BG34_Giant_120_G ? 2 : 1;
-		addStatsToBoard(entity, input.board, input.hero, 3 * mult, 1 * mult, input.gameState);
+		addStatsToBoard(entity, input.board, input.hero, 3 * mult, 1 * mult, input.gameState, Race[Race.MECH]);
 	},
 };
