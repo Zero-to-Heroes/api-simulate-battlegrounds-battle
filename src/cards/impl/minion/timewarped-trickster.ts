d5ec68710c8a0c58a47a16209bd686de91f9f1d3
sebastientromp <sebastien.tromp@gmail.com>
2025-12-03 10:24:01 +0100

    fix timewarped trickster

--- src/cards/impl/minion/timewarped-trickster.ts
+++ src/cards/impl/minion/timewarped-trickster.ts
@@ -1,6 +1,6 @@
 import { BoardEntity } from '../../../board-entity';
 import { CardIds } from '../../../services/card-ids';
-import { getNeighbours } from '../../../simulation/attack';
+import { pickRandomAlive } from '../../../services/utils';
 import { DeathrattleTriggeredInput } from '../../../simulation/deathrattle-on-trigger';
 import { modifyStats } from '../../../simulation/stats';
 import { DeathrattleSpawnCard } from '../../card.interface';
@@ -9,14 +9,10 @@ export const TimewarpedTrickster: DeathrattleSpawnCard = {
 	cardIds: [CardIds.TimewarpedTrickster_BG34_Giant_010, CardIds.TimewarpedTrickster_BG34_Giant_010_G],
 	deathrattleSpawn: (minion: BoardEntity, input: DeathrattleTriggeredInput) => {
 		const mult = minion.cardId === CardIds.TimewarpedTrickster_BG34_Giant_010_G ? 2 : 1;
-		const hasImpulsivePortrait = input.boardWithDeadEntityHero.trinkets?.some(
-			(t) => t.cardId === CardIds.ImpulsivePortrait_BG32_MagicItem_820,
-		);
-		const targets = hasImpulsivePortrait
-			? getNeighbours(input.boardWithDeadEntity, input.deadEntity, input.deadEntityIndexFromRight)
-			: input.boardWithDeadEntity.filter((e) => e != minion);
-		for (let j = 0; j < mult; j++) {
-			for (const target of targets) {
+		const targets = input.boardWithDeadEntity.filter((e) => e != minion);
+		const target = pickRandomAlive(targets);
+		if (!!target) {
+			for (let j = 0; j < mult; j++) {
 				modifyStats(
 					target,
 					minion,
