172fc92819c84dc53181aae66bf21d0a93ad3333
sebastientromp <sebastien.tromp@gmail.com>
2025-12-04 14:10:22 +0100

    fix bluesy siren

--- src/cards/impl/minion/bluesy-siren.ts
+++ src/cards/impl/minion/bluesy-siren.ts
@@ -4,40 +4,47 @@ import { castTavernSpell } from '../../../mechanics/cast-tavern-spell';
 import { CardIds } from '../../../services/card-ids';
 import { OnAttackInput } from '../../../simulation/on-attack';
 import { hasCorrectTribe } from '../../../utils';
-import { DefaultChargesCard, OnWheneverAnotherMinionAttacksCard } from '../../card.interface';
+import { DefaultChargesCard, OnWheneverAnotherMinionAttacksCard, RallyCard } from '../../card.interface';
 
-export const BluesySiren: OnWheneverAnotherMinionAttacksCard & DefaultChargesCard = {
+export const BluesySiren: OnWheneverAnotherMinionAttacksCard & RallyCard & DefaultChargesCard = {
 	cardIds: [CardIds.BluesySiren_BG34_931, CardIds.BluesySiren_BG34_931_G],
 	defaultCharges: (entity: BoardEntity) => 3,
 	onWheneverAnotherMinionAttacks: (minion: BoardEntity, input: OnAttackInput) => {
-		if (
-			!hasCorrectTribe(
-				input.attacker,
-				input.attackingHero,
-				Race.NAGA,
-				input.gameState.anomalies,
-				input.gameState.allCards,
-			)
-		) {
-			return { dmgDoneByAttacker: 0, dmgDoneByDefender: 0 };
-		}
-
-		minion.abiityChargesLeft = minion.abiityChargesLeft - 1;
-		const spellCast =
-			minion.cardId === CardIds.BluesySiren_BG34_931_G
-				? CardIds.DeepBlueCrooner_DeepBluesToken_BG26_502_Gt
-				: CardIds.DeepBlueCrooner_DeepBluesToken_BG26_502t;
-		castTavernSpell(spellCast, {
-			spellCardId: spellCast,
-			source: input.attackingHero,
-			target: input.attacker,
-			board: input.attackingBoard,
-			hero: input.attackingHero,
-			otherBoard: input.defendingBoard,
-			otherHero: input.defendingHero,
-			gameState: input.gameState,
-		});
-
-		return { dmgDoneByAttacker: 0, dmgDoneByDefender: 0 };
+		return process(minion, input);
 	},
+	rally: (minion: BoardEntity, input: OnAttackInput) => {
+		return process(minion, input);
+	},
+};
+
+const process = (minion: BoardEntity, input: OnAttackInput) => {
+	if (
+		!hasCorrectTribe(
+			input.attacker,
+			input.attackingHero,
+			Race.NAGA,
+			input.gameState.anomalies,
+			input.gameState.allCards,
+		)
+	) {
+		return { dmgDoneByAttacker: 0, dmgDoneByDefender: 0 };
+	}
+
+	minion.abiityChargesLeft = minion.abiityChargesLeft - 1;
+	const spellCast =
+		minion.cardId === CardIds.BluesySiren_BG34_931_G
+			? CardIds.DeepBlueCrooner_DeepBluesToken_BG26_502_Gt
+			: CardIds.DeepBlueCrooner_DeepBluesToken_BG26_502t;
+	castTavernSpell(spellCast, {
+		spellCardId: spellCast,
+		source: input.attackingHero,
+		target: input.attacker,
+		board: input.attackingBoard,
+		hero: input.attackingHero,
+		otherBoard: input.defendingBoard,
+		otherHero: input.defendingHero,
+		gameState: input.gameState,
+	});
+
+	return { dmgDoneByAttacker: 0, dmgDoneByDefender: 0 };
 };
