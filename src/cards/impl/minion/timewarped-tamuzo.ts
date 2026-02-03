761a594846afe8576781dfc2a3c7dab6d4c7d42d
sebastientromp <sebastien.tromp@gmail.com>
2026-02-03 16:53:27 +0100

    fix timewarped tamuzo

--- src/cards/impl/minion/timewarped-tamuzo.ts
+++ src/cards/impl/minion/timewarped-tamuzo.ts
@@ -1,18 +1,26 @@
 import { BoardEntity } from '../../../board-entity';
 import { CardIds } from '../../../services/card-ids';
 import { OnOtherSpawnInput } from '../../../simulation/add-minion-to-board';
-import { modifyStats } from '../../../simulation/stats';
+import { setEntityStats } from '../../../simulation/stats';
 import { AfterOtherSpawnedCard } from '../../card.interface';
 
 export const TimewarpedTamuzo: AfterOtherSpawnedCard = {
 	cardIds: [CardIds.TimewarpedTamuzo_BG34_Giant_595, CardIds.TimewarpedTamuzo_BG34_Giant_595_G],
 	afterOtherSpawned: (minion: BoardEntity, input: OnOtherSpawnInput) => {
 		const mult = minion.cardId === CardIds.TimewarpedTamuzo_BG34_Giant_595_G ? 2 : 1;
-		modifyStats(
+		// modifyStats(
+		// 	input.spawned,
+		// 	minion,
+		// 	input.spawned.attack * mult,
+		// 	input.spawned.maxHealth * mult,
+		// 	input.board,
+		// 	input.hero,
+		// 	input.gameState,
+		// );
+		setEntityStats(
 			input.spawned,
-			minion,
-			input.spawned.attack * mult,
-			input.spawned.maxHealth * mult,
+			input.spawned.attack + input.spawned.attack * mult,
+			input.spawned.health + input.spawned.maxHealth * mult,
 			input.board,
 			input.hero,
 			input.gameState,
--- test/full-game/apply-debug-state.ts
+++ test/full-game/apply-debug-state.ts
@@ -2,7 +2,10 @@ import { debugState } from '../../src/debug-state';
 
 export const applyDebugState = () => {
 	const state = debugState;
-	state.active = true;
+	state.active = false;
 	state.forcedCurrentAttacker = 0;
-	state.forcedFaceOffBase = [{ attacker: { entityId: 12259 }, defender: { entityId: 9579 } }];
+	state.forcedFaceOffBase = [
+		{ attacker: { entityId: 12259 }, defender: { entityId: 9579 } },
+		{ attacker: { attack: 3, health: 1 }, defender: { entityId: 7712 } },
+	];
 };
